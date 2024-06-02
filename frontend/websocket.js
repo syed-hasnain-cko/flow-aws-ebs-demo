a10a630979ca506fb3b518730b544c8a633d9dce
syed-hasnain-cko <91118680+syed-hasnain-cko@users.noreply.github.com>
2024-06-02 04:21:02 +0200

    payment data update on incoming webhook message - show toast

--- frontend/websocket.js
+++ frontend/websocket.js
@@ -13,8 +13,15 @@ ws.onopen = () => {
 ws.onmessage = event => {
     const message = JSON.parse(event.data);
     console.log('Received message:', message);
-
+if(message.type != 'payment_declined' || message.type != 'payment_canceled' || message.type != 'payment_void_declined' 
+|| message.type != 'payment_capture_declined' || message.type != 'payment_refund_declined' || message.type != 'payout_declined'
+|| message.type != 'payment_expired'){
     updatePaymentDetailsData(message.data.id);
+    showToast(`${message.type} success confirmation`)
+}
+    else{
+        showToast(`${message.type} action - Failure`,false)
+    }
 
 };
 
