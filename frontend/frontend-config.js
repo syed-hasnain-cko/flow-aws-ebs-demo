b08d9cf655891b0638146106a4024553d47e8be4
syed-hasnain-cko <91118680+syed-hasnain-cko@users.noreply.github.com>
2026-03-23 18:12:44 +0100

    added enviroment variables to fetch sk, pk and api_url of aws-api

---
+++ frontend/frontend-config.js
@@ -0,0 +1,7 @@
+    window.APP_CONFIG = {
+        apiBaseUrl: "https://zzrte604h4.execute-api.us-east-1.amazonaws.com/staging",
+        processingChannelId: "pc_oxr4t4p3nseejeqdjqk3pdlpm4",
+        publicKey: "pk_sbox_7za2ppcb4pw7zzdkfzutahfjl4t",
+        googleMerchantId: "12345678901234567890",
+        appleMerchantId: "merchant.sandbox.syed"
+    };
